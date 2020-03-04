06b0b0312ac04b47a0194e9e5d66284732af03bc
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-03 19:43:37 -0500

    connection made, authorization needed

--- src/main/java/commands/SpotifySearch.java
+++ src/main/java/commands/SpotifySearch.java
@@ -3,22 +3,46 @@ package commands;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SpotifySearch extends ListenerAdapter {
 
     public void onGuildMessageReceived (GuildMessageReceivedEvent event){
         String[] messageSent = event.getMessage().getContentRaw().split(" ");
-
-        if(messageSent[0].equalsIgnoreCase("!spotbot") && messageSent.length >= 2){
-            StringBuilder sb = new StringBuilder();
-            for(int i = 1; i < messageSent.length; i++){
-                sb.append(messageSent[i]);
-                sb.append("%20");
+        if(messageSent[0].equalsIgnoreCase("!spotbot") && messageSent.length >= 2) {
+            try {
+                makeAPICall(messageSent, event);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            String spotifySearchURL = "https://api.spotify.com/v1/search?q=";
-            String searchQuery = sb.toString().substring(0,sb.toString().length() - 3).toLowerCase();
-            String queryType = "&type=track";
-            event.getChannel().sendMessage(spotifySearchURL + searchQuery + queryType).queue();
         }
 
+    }
+
+    private void makeAPICall(String[] messageSent, GuildMessageReceivedEvent event) throws IOException {
+        URL url = new URL(getRequestURL(messageSent));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        StringBuilder fullResponse = new StringBuilder();
+        fullResponse.append(connection.getResponseCode())
+                .append(" ")
+                .append(connection.getResponseMessage())
+                .append("\n");
+
+        event.getChannel().sendMessage(fullResponse.toString()).queue();
+    }
+
+    private String getRequestURL(String[] messageSent) {
+        String spotifySearchURL = "https://api.spotify.com/v1/search?q=";
+        StringBuilder sb = new StringBuilder();
+        String queryType = "&type=track";
+        for(int i = 1; i < messageSent.length; i++){
+            sb.append(messageSent[i]);
+            sb.append("%20");
+        }
+        String searchQuery = sb.toString().substring(0,sb.toString().length() - 3).toLowerCase();
+        return spotifySearchURL + searchQuery + queryType;
     }
 }
