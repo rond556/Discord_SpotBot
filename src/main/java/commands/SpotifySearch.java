4f6e1035edb5ec9da77d6ae676e36de297b0d0f9
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-04 19:32:14 -0500

    better API call

--- src/main/java/commands/SpotifySearch.java
+++ src/main/java/commands/SpotifySearch.java
@@ -2,9 +2,12 @@ package commands;
 
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class SpotifySearch extends ListenerAdapter {
@@ -24,22 +27,22 @@ public class SpotifySearch extends ListenerAdapter {
 
     private void makeAPICall(String[] messageSent, GuildMessageReceivedEvent event) throws IOException {
         URL url = new URL(getRequestURL(messageSent));
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Authorization", "Bearer " + token);
-        StringBuilder fullResponse = new StringBuilder();
-        fullResponse.append(connection.getResponseCode())
-                .append(" ")
-                .append(connection.getResponseMessage())
-                .append("\n");
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization","Bearer " + token)
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        String jsonResponse = response.body().string();
+        System.out.println(jsonResponse);
 
-        event.getChannel().sendMessage(fullResponse.toString()).queue();
     }
 
     private String getRequestURL(String[] messageSent) {
         String spotifySearchURL = "https://api.spotify.com/v1/search?q=";
         StringBuilder sb = new StringBuilder();
-        String queryType = "&type=track";
+        String queryType = "&type=track&limit=1";
         for(int i = 1; i < messageSent.length; i++){
             sb.append(messageSent[i]);
             sb.append("%20");
