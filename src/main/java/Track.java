fcc439dfed316846dfe781e4143e2c7eded403cb
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-04 19:34:37 -0500

    track model

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,2 +1,38 @@
 public class Track {
+    private String title;
+    private String artist;
+    private String urlLink;
+
+    public Track() {
+    }
+
+    public Track(String title, String artist, String urlLink) {
+        this.title = title;
+        this.artist = artist;
+        this.urlLink = urlLink;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getUrlLink() {
+        return urlLink;
+    }
+
+    public void setUrlLink(String urlLink) {
+        this.urlLink = urlLink;
+    }
 }
