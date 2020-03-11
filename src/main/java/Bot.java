76ee0fe0ab719977731a5f41def7149fa8aeb000
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-03-11 15:33:16 -0400

    fixed tokens

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Bot {
 
     public static void main(String args[]) throws Exception{
-        JDA jda = new JDABuilder("Njg0MTUzODQwNzM5NTQ5MjA0.XmZacg.8efmhcHlNm7jrEsnpkuI3v0aiJY").build();
+        JDA jda = new JDABuilder("Njg0MTUzODQwNzM5NTQ5MjA0.Xmk8Pw.4b_m7AUyNZ3TLMi4dEaJtiz1qN8").build();
 
         jda.addEventListener(new SpotifySearch());
     }
